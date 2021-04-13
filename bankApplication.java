7e3e8010e166acbf82997172348b537158e5cbd2
Carter Facey-Smith <43100930+CarterFaceySmith@users.noreply.github.com>
2021-04-13 17:09:31 +1000

    Add files via upload
    - Basic code structure done
    - Basic signup process done
    - User class complete

--- bankApplication.java
+++ bankApplication.java
@@ -1,27 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class bankApplication {
 public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
 
     boolean running = true;
-    System.out.println("Welcome to ShitBank(all rights reserved), the world's leading shitty bank.")
-    System.out.println("If you are a new user, please type 'sign up', otherwise type 'login'");
-    String userInput = scnr.nextLine();
-        
-        BANK:
-        while(running){
 
-        
+    BANK:
+    while(running){
 
-        if (userInput.equals("sign up")){
-            //Sign up process
-        }
+            System.out.println("Welcome to Bank(all rights reserved), the world's leading bank.\n");
+            System.out.println("If you are a new user, please type 'sign up', otherwise type 'login'\n");
+            String userInput = scnr.nextLine();
 
-        else if (userInput.equals("login")){
-            //login process
-        }
-        System.out.println("Please enter your username ");
+            if (userInput.equals("sign up")){
+
+                //Sign up process
+                System.out.println("Please enter a username: ");
+                String nameTemp = scnr.nextLine();
+                System.out.println();
+                System.out.println("Please enter a password: ");
+                String wordTemp = scnr.nextLine();
+                System.out.println();
+                System.out.println("Please enter your external current wallet amount to two decimal places: ");
+                Double walletTemp = scnr.nextDouble();
+                System.out.println();
+                System.out.println("Your account has been opened successfully.\n");
+                System.out.println("Thank you for opening an account with Bank(all rights reserved), we recommend you store your information in a safe location.\n");
+                continue BANK;
+            }
+
+            else if (userInput.equals("login")){
+
+                //login process
+                System.out.println("Please enter your username: \n");
         
+            }
+        
+            System.out.print("End of program.");
+
         }
 
 
